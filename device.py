78182bcf3414ad3c39b85fc8f15b496b9a800f80
PaoloB98 <paolobono98@gmail.com>
2023-10-22 15:26:46 +0200

    -

--- device.py
+++ device.py
@@ -1,19 +1,22 @@
 import logging
-
 import requests
 import time
 
+logger = logging.getLogger("Device")
+
+
 class Device():
-    url_for_requests: str # URL used for requesting
+    url_for_requests: str  # URL used for requesting
 
     def __init__(self, url_for_request):
         super().__init__()
         self.url_for_requests = url_for_request
 
     def continuous_requesting(self, time_seconds: int, interval_between_req: int):
-        n_of_request = int(time_seconds/interval_between_req)
+        n_of_request = int(time_seconds / interval_between_req)
 
-        for request_number in range(1,n_of_request):
-            logging.info(f"Request n. {request_number}")
+        for request_number in range(1, n_of_request):
+            logger.info(f"Request n. {request_number}")
             result = requests.get(url=self.url_for_requests)
+            logger.info(f"Response status: {result.status_code}\n----\n")
             time.sleep(interval_between_req)
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import logging
+import sys
+from device import Device
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Main")
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def main() -> int:
+    logger.info("Starting...")
+    # Qua è dove parte l'eseguzione del software
+    device_n1 = Device(url_for_request="http://example.com")
+    # Richieste continue, vedi console per codice stato richieste.
+    device_n1.continuous_requesting(time_seconds=30, interval_between_req=5)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
